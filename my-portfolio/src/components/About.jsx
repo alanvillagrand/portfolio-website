1ed7c1f17cb2ad77daf2cc53ca00cebd582b5803
Alan Villagrand <alan_villagrand1@baylor.edu>
2024-07-31 00:00:32 -0500

    Added about section

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -1,9 +1,25 @@
+import '../styles/About.css'
+import { FaFileDownload } from 'react-icons/fa'
 
 function About() {
     return (
         <section id="about">
-            <h1>About Me</h1>
-            <p>This is the about me section</p>
+            <div className="about-text">
+                <h1 className='about-heading'>About Me</h1>
+                <p className='about-paragraph'>
+                    Hi, my name is Alan Villagrand. I'm a passionate and driven computer science student
+                    at Baylor University. I love to create innovative applications and solve complex problems.
+                    With a strong foundation in data strucures, algorithms, and programing technologies, I am eager
+                    to continously learn and excel in software engineering
+                </p>
+                <p className='about-paragraph'>
+                    I am actively seeking new opportunities, especially for the summer of 2025. Feel free to contact me here.
+                    Apart from my career interests, I love to play soccer, workout, enjoy the outdoors, and meet new people!
+                </p>
+                <button className='resume-button'>
+                    <span className="button-content">Resume<FaFileDownload /></span>
+                </button>
+            </div>
         </section>
     );
 }
