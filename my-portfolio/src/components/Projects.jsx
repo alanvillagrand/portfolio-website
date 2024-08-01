e13740e5394a590047e094f61ba0b182cc068cdf
Alan Villagrand <alan_villagrand1@baylor.edu>
2024-08-01 01:23:29 -0500

    Added sorting algorithm project card

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,16 +1,29 @@
 import '../styles/Projects.css'
-import ProjectCard from "./ProjectCard";
-import SortingPicture from "../assets/sorting-project.png"
+import ProjectCard from './ProjectCard'
+import SortingPicture from '../assets/sorting-project.png'
+import HotelPicture from '../assets/hotel-project.png'
 
 function Projects() {
 
-    const sortingDescription = "A Java program that visualizes common sorting algorithms. " +
-                               "This project helped me learn the basics of Java and the Java Swing library.";
+    const sortingDescription = `A Java program that visualizes common sorting algorithms.
+                                This project helped me learn the basics of Java and the 
+                                Java Swing library while also enhancing my understanding of
+                                essential sorting algorithms.`;
+    
+    const hotelDescription = `A hotel reservation system developed using Java that provides a
+                              user-friendly experience to simplify the hotel booking process. I
+                              learned how to collaborate with other developers and implement an
+                              H2 database schema for efficient storage and management of reservation
+                              data.`;
 
     return (
         <section id="projects">
             <h1 className="projects-heading">My Projects</h1>
             <div className="projects-container">
+                <ProjectCard picture={HotelPicture}
+                             title="Hotel Reservation System"
+                             description={hotelDescription}
+                             githubLink="https://github.com/alanvillagrand/CSI-3471-group-project"/>
                 <ProjectCard picture={SortingPicture} 
                              title="Sorting Algorithm Visualizer"
                              description={sortingDescription}
