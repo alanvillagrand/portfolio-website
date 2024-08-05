72aa55d02fc26b5de54469c96b7001d6e511b63d
Alan Villagrand <alan_villagrand1@baylor.edu>
2024-08-04 22:33:20 -0500

    Fixed contact styling issues

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Contact.css'
 import { FaCheck } from 'react-icons/fa'
 import LinkedinIcon from '../assets/linkedin.svg'
@@ -6,6 +6,8 @@ import MailIcon from '../assets/mail.svg'
 import PhoneIcon from '../assets/phone.svg'
 
 function Contact() {
+
+    const [submitted, setSubmitted] = useState(false);
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -27,6 +29,7 @@ function Contact() {
     
         if (res.success) {
           console.log("Success", res);
+          setSubmitted(true);
         }
       };
 
@@ -48,24 +51,33 @@ function Contact() {
                         <p>281-455-7606</p>
                     </div>
                 </div>
-                <h1>Get In Touch</h1>
-                <form className='contact-form'>
-                    <div className="input-box">
-                        <label htmlFor='name'>Full Name</label>
-                        <input type="text" id='name' name="name" className="field" placeholder="Enter your name" required/>
-                    </div>
-                    <div className="input-box">
-                        <label htmlFor='email'>Email Address</label>
-                        <input type="email" id='email' name='email' className="field" placeholder="Enter your email" required/>
-                    </div>
-                    <div className="input-box">
-                        <label htmlFor='message'>Your Message</label>
-                        <textarea name="message" className="field area" placeholder="Enter your message" required></textarea>
-                    </div>
-                    <button type="submit">
-                        <span className="button-content">Submit <FaCheck /></span>
-                    </button>
-                </form>
+                {submitted ? (
+                    <>
+                        <h1>Thank You!</h1>
+                        <p className='submitted-message'>Your form has been submitted.</p>
+                    </>
+                ) : (
+                    <>
+                        <h1>Get In Touch</h1>
+                        <form onSubmit={onSubmit} className='contact-form'>
+                            <div className="input-box">
+                                <label htmlFor='name'>Full Name</label>
+                                <input type="text" id='name' name="name" className="field" placeholder="Enter your name" required/>
+                            </div>
+                            <div className="input-box">
+                                <label htmlFor='email'>Email Address</label>
+                                <input type="email" id='email' name='email' className="field" placeholder="Enter your email" required/>
+                            </div>
+                            <div className="input-box">
+                                <label htmlFor='message'>Your Message</label>
+                                <textarea name="message" className="field area" placeholder="Enter your message" required></textarea>
+                            </div>
+                            <button type="submit">
+                                <span className="button-content">Submit <FaCheck /></span>
+                            </button>
+                        </form>
+                    </>
+                )}
             </div>
         </section>
     );
