ee4689cd52c51fe05c16c5fdcc6b5ef0af104da6
Alan Villagrand <alan_villagrand1@baylor.edu>
2024-08-06 16:05:31 -0500

    fix home page styling issues

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -5,6 +5,11 @@ import { CgPhone } from "react-icons/cg";
 import { BsPerson } from "react-icons/bs";
 
 function Home() {
+
+    const navigateTo = (targetId) => {
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth'});
+    };
+
     return(
         <section id="home">
             <div className="home-text">
@@ -12,10 +17,10 @@ function Home() {
                 <h1>I'm <span className="name">Alan Villagrand</span></h1>
                 <Type />
                 <div className="home-buttons">
-                    <button>
+                    <button onClick={() => navigateTo('about')}>
                         <span className="button-content">About Me <BsPerson /></span>
                     </button>
-                    <button>
+                    <button onClick={() => navigateTo('contact')}>
                         <span className="button-content">Contact <CgPhone /></span>
                     </button>
                 </div>
